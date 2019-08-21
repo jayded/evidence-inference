46a6d67a6d55048d49296285019e50a15a569f87
Eric Lehman <lehmer16@gmail.com>
2019-08-21 16:12:03 -0400

    update documentation regarding changes made
    Add documentation to the README to describe what the differences are in
    the annotations between this, and the last version. Additionally, add a
    comment describing why we are ignoring title tags.

--- evidence_inference/preprocess/article_reader.py
+++ evidence_inference/preprocess/article_reader.py
@@ -115,7 +115,7 @@ class Article:
         parser = MyHTMLParser()      
         parser_table = TableHTMLParser()
     
-        section_name = self._get_section_name(start_node) # 900 - 1000
+        section_name = self._get_section_name(start_node)
         if parent_section_str is not None:
             if section_name is None:
                 section_name = parent_section_str
@@ -138,14 +138,14 @@ class Article:
                 to_app = parser_table.get_data()
                 self.article_dict[section_name].append(to_app)
             elif section_type == 'title':
+                # we don't need to include title headers because we already extract, 
+                # and use them to join strings (including would only double them).
                 continue
             else:
                 txt = ET.tostring(element).decode("utf-8")
                 parser.feed(txt)
                 txt = fmt(parser.get_data())
                 self.article_dict[section_name].append(txt)
-
-                
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
