5d5fca4d3230c5830db198bcf4eb2f4f214c862b
Jay DeYoung <deyoung.j@husky.neu.edu>
2020-06-15 08:44:45 -0400

    QOL improvements to PaddedSequence code

--- evidence_inference/models/utils.py
+++ evidence_inference/models/utils.py
@@ -1,39 +1,31 @@
 from collections import namedtuple
+from dataclasses import dataclass
 
 from typing import List
 
 import torch
 from torch.nn.utils.rnn import pad_sequence, PackedSequence, pack_padded_sequence, pad_packed_sequence
 
-PaddedSequence_ = namedtuple('PaddedSequence_', ['data', 'batch_sizes'])
-
-
-class PaddedSequence(PaddedSequence_):
+@dataclass(eq=True, frozen=True)
+class PaddedSequence:
     """A utility class for padding variable length sequences mean for RNN input
-
     This class is in the style of PackedSequence from the PyTorch RNN Utils,
     but is somewhat more manual in approach. It provides the ability to generate masks
     for outputs of the same input dimensions.
-
     The constructor should never be called directly and should only be called via
     the autopad classmethod.
 
-    This originated because PyTorch's PackedPadded originally required sorting by length,
-    however this need is no longer met. We looked into removing this class and unfortunately
-    it seems that it's still needed. We would very much like to remove this code, but until
-    PackedSequences are treated as first-class funny-looking tensors, that appears to be
-    impossible.
+    We'd love to delete this, but we pad_sequence, pack_padded_sequence, and
+    pad_packed_sequence all require shuffling around tuples of information, and some
+    convenience methods using these are nice to have.
     """
 
-    def __new__(cls, data: torch.Tensor, batch_sizes: torch.Tensor, batch_first=False):
-        self = super(PaddedSequence, cls).__new__(cls, data, batch_sizes)
-        self.batch_first = batch_first
-        if not batch_first:
-            raise ValueError("No models in this file work without batch_first!")
-        return self
+    data: torch.Tensor
+    batch_sizes: torch.Tensor
+    batch_first: bool=True
 
     @classmethod
-    def autopad(cls, data, batch_first: bool=False, padding_value=0) -> 'PaddedSequence':
+    def autopad(cls, data, batch_first: bool=True, padding_value=0, device=None) -> 'PaddedSequence':
         padded = pad_sequence(data, batch_first=batch_first, padding_value=padding_value)
         if batch_first:
             batch_lengths = torch.LongTensor([len(x) for x in data])
@@ -42,7 +34,7 @@ class PaddedSequence(PaddedSequence_):
         else:
             # TODO actually test this codepath
             batch_lengths = torch.LongTensor([len(x) for x in data])
-        return PaddedSequence(padded, batch_lengths, batch_first)
+        return PaddedSequence(padded, batch_lengths, batch_first).to(device=device)
 
     def pack_other(self, data: torch.Tensor):
         return pack_padded_sequence(data, self.batch_sizes, batch_first=self.batch_first, enforce_sorted=False)
@@ -55,21 +47,33 @@ class PaddedSequence(PaddedSequence_):
     def cuda(self) -> 'PaddedSequence':
         return PaddedSequence(self.data.cuda(), self.batch_sizes.cuda(), batch_first=self.batch_first)
 
-    def mask(self, cuda=False) -> torch.Tensor:
-        out_tensor = torch.zeros(*self.data.size()[0:2])
+    def to(self, dtype=None, device=None, copy=False, non_blocking=False) -> 'PaddedSequence':
+        # TODO make to() support all of the torch.Tensor to() variants
+        return PaddedSequence(
+                    self.data.to(dtype=dtype, device=device, copy=copy, non_blocking=non_blocking),
+                    self.batch_sizes.to(device=device, copy=copy, non_blocking=non_blocking),
+                    batch_first=self.batch_first)
+
+    def mask(self, on, off, device='cpu', dtype=None, size=None) -> torch.Tensor:
+        if size is None:
+            size = self.data.size()
+        out_tensor = torch.zeros(*size, dtype=dtype)
+        # TODO this can be done more efficiently
+        out_tensor.fill_(off)
+        # note to self: these are probably less efficient than explicilty populating the off values instead of the on values.
         if self.batch_first:
             for i, bl in enumerate(self.batch_sizes):
-                out_tensor[i, :bl] = torch.ones(bl)
+                out_tensor[i, :bl] = on
         else:
             for i, bl in enumerate(self.batch_sizes):
-                out_tensor[:bl, i] = torch.ones(bl)
-        if cuda:
-            return out_tensor.cuda()
-        else:
-            return out_tensor
+                out_tensor[:bl, i] = on
+        return out_tensor.to(device)
 
     def unpad(self, other: torch.Tensor) -> List[torch.Tensor]:
         out = []
         for o, bl in zip(other, self.batch_sizes):
             out.append(o[:bl])
         return out
+
+    def flip(self) -> 'PaddedSequence':
+        return PaddedSequence(self.data.transpose(0,1), not self.batch_first, self.padding_value)
