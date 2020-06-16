7a4acebaee94124d9339baed4f321c5b9a6ce507
bwallace <byron.wallace@gmail.com>
2020-06-16 15:58:44 -0400

    updates to bioRoBERTa script

--- scripts/train_bioRoBERTa.py
+++ scripts/train_bioRoBERTa.py
@@ -1,6 +1,8 @@
 '''
 Train a RoBERTa-based (unconditional!) punchline extractor. 
-This is a simple helper 
+This is a simple model that operates over sentences,
+tagging as `evidence bearing` or not. Uses negative
+sampling.  
 '''
 
 import random 
@@ -12,6 +14,7 @@ from transformers import RobertaForSequenceClassification, RobertaTokenizer, Pre
 import sys
 sys.path.append("../")
 from evidence_inference.preprocess.preprocessor import get_Xy, train_document_ids, test_document_ids, validation_document_ids, get_train_Xy                                                                    
+device = torch.device('cuda')
 
 tr_ids = list(train_document_ids()) 
 train_Xy, inference_vectorizer = get_train_Xy(tr_ids, sections_of_interest=None, 
@@ -20,7 +23,7 @@ train_Xy, inference_vectorizer = get_train_Xy(tr_ids, sections_of_interest=None,
 
 
 val_ids = list(validation_document_ids())
-val_Xy  = get_Xy(val_ids[:10], inference_vectorizer, include_raw_texts=True) 
+val_Xy  = get_Xy(val_ids, inference_vectorizer, include_raw_texts=True) 
 
 
 def instances_from_article(article_dict, neg_samples=2, max_instances=6):
@@ -50,8 +53,8 @@ def instances_from_article(article_dict, neg_samples=2, max_instances=6):
 
 def train(train_Xy, n_epochs=4, batch_size=4): # val_Xy
     tokenizer = RobertaTokenizer.from_pretrained("allenai/biomed_roberta_base") 
-    model     = RobertaForSequenceClassification.from_pretrained("allenai/biomed_roberta_base") 
-
+    model     = RobertaForSequenceClassification.from_pretrained("allenai/biomed_roberta_base").to(device=device) 
+    
     from transformers import AdamW
     optimizer = AdamW(model.parameters())
 
@@ -70,44 +73,51 @@ def train(train_Xy, n_epochs=4, batch_size=4): # val_Xy
             cur_batch_size = 0
             while cur_batch_size < batch_size:
                 cur_X, cur_y = instances_from_article(article, max_instances=batch_size-cur_batch_size)
+                if len(cur_X) == 0:
+                    print("no positive instances in article {}".format(i))
+                    break
                 batch_X.extend(cur_X)
                 batch_y.extend(cur_y)
 
                 cur_batch_size += len(cur_X)
 
-            # 
-            model.zero_grad()  
-
-            batch_X_tensor = tokenizer.batch_encode_plus(batch_X, add_special_tokens=True, pad_to_max_length=True) #tokenizer.encode_plus(batch_X[:batch_size], add_special_tokens=True)
-            batch_y_tensor = torch.tensor(batch_y)
-            
-     
-            loss, logits  = model(torch.tensor(batch_X_tensor['input_ids']), 
-                                  attention_mask=torch.tensor(batch_X_tensor['attention_mask']), 
-                                  labels=torch.tensor(batch_y))
-
-            print(loss)
-            loss.backward()
-            optimizer.step()
+            if len(batch_X) == 0:
+                pass
+            else:
+                model.zero_grad()  
+                
+                batch_X_tensor = tokenizer.batch_encode_plus(batch_X, max_length=512, add_special_tokens=True, pad_to_max_length=True) #tokenizer.encode_plus(batch_X[:batch_size], add_special_tokens=True)
+                batch_y_tensor = torch.tensor(batch_y)
+                loss, logits  = model(torch.tensor(batch_X_tensor['input_ids']).to(device=device), 
+                                  attention_mask=torch.tensor(batch_X_tensor['attention_mask']).to(device=device), 
+                                  labels=torch.tensor(batch_y).to(device=device))
+                #print(loss)
+                loss.backward()
+                optimizer.step()
 
         ####
         # eval on val set
         ###
         print("evaluating on val...")
         model.eval()
+        total_correct, total_preds = 0, 0
         val_loss = 0
         for j, article in enumerate(val_Xy):
             val_X, val_y = instances_from_article(article, max_instances=batch_size)
             val_X_tensor = tokenizer.batch_encode_plus(val_X, add_special_tokens=True, pad_to_max_length=True) #tokenizer.encode_plus(batch_X[:batch_size], add_special_tokens=True)
             val_y_tensor = torch.tensor(val_y)
             
-            loss, logits  = model(torch.tensor(val_X_tensor['input_ids']), 
-                                  attention_mask=torch.tensor(val_X_tensor['attention_mask']), 
-                                  labels=torch.tensor(val_y_tensor))
+            loss, logits  = model(torch.tensor(val_X_tensor['input_ids']).to(device=device), 
+                                  attention_mask=torch.tensor(val_X_tensor['attention_mask']).to(device=device), 
+                                  labels=torch.tensor(val_y_tensor).to(device=device))
+            val_loss += loss.cpu().detach().numpy()
             
-            val_loss += loss.detach().numpy()
-        
-        print("val loss after epoch {} is: {}".format(epoch, val_loss))
+            class_preds = torch.argmax(logits, dim=1).detach().cpu()
+            total_correct += (class_preds == val_y_tensor).sum()
+            total_preds += len(val_X)
+     
+        val_acc = total_correct / float(total_preds) # note that the baseline depends on neg samples
+        print("val loss, acc after epoch {} is: {}, {}".format(epoch, val_loss, val_acc))
         if val_loss < best_val:
             print("new best loss: {}".format(val_loss))
             best_val = val_loss
